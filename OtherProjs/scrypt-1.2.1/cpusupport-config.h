6f1f71f0955d00000c1f2c7a05324bf1ea4e2fc6
Runchao Han <408063141@qq.com>
2017-12-28 20:39:43 +0000

    1

--- OtherProjs/scrypt-1.2.1/cpusupport-config.h
+++
@@ -1,6 +0,0 @@
-#define CPUSUPPORT_X86_CPUID 1
-#define CPUSUPPORT_X86_SSE2 1
-#define CPUSUPPORT_X86_AESNI 1
-#ifdef cpusupport_dummy
-export CFLAGS_X86_AESNI="-maes"
-#endif
--- tests/1.c
+++
@@ -1,7 +0,0 @@
-#include "omp.h"
-#include "stdio.h"
-
-int main(int argc, char const *argv[]) {
-  printf("%s\n", "fuck you");
-  return 0;
-}
